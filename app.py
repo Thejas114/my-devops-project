d6176d6562dbc0ab8c9ea885403de1f3f8411b58
Thejas114 <thejasnaika587c@gmail.com>
2026-05-23 13:55:36 +0530

    updated flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Argo CD Auto Deploy Success 🚀"
+    return "GitOps Auto Deployment Success!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
